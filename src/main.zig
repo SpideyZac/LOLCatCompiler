575386690a38bacec99b512f9d83ccf440fee7de
spideyzac <zacmlesser-7@outlook.com>
2024-05-03 18:31:40 -0700

    feat: bukkit type (temp)

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,48 @@
 // https://ziglang.org/documentation/master/#Generic-Data-Structures
 
 const std = @import("std");
+const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
     std.log.info("hello from tempLang!", .{});
+
+    var b = Bukkit{
+        .values = std.ArrayList(BukkitValue).init(allocator),
+    };
+
+    try b.values.append(BukkitValue{ .yarn = std.ArrayList(u8).init(allocator) });
+    try b.values.items[0].yarn.appendSlice("asd");
+    std.log.info("b.values[0].yarn: {s}", .{b.values.items[0].yarn.items});
+    defer b.deinit();
 }
 
-fn List(comptime T: type) type {
-    return struct {
-        items: []T,
-        len: usize,
-    };
-}
+// To move later just testing
+const BukkitValueTag = enum {
+    yarn,
+    number,
+    numbar,
+    troof,
+    noob,
+    bukkit,
+};
+const BukkitValue = union(BukkitValueTag) {
+    yarn: std.ArrayList(u8),
+    number: i64,
+    numbar: f64,
+    troof: bool,
+    noob: void,
+    bukkit: *Bukkit,
+};
+const Bukkit = struct {
+    values: std.ArrayList(BukkitValue),
+    fn deinit(self: *Bukkit) void {
+        for (self.values.items) |value| {
+            switch (value) {
+                .yarn => value.yarn.deinit(),
+                .bukkit => value.bukkit.deinit(),
+                else => {},
+            }
+        }
+        self.values.deinit();
+    }
+};
