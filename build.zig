20c948acf569a2ff4c4d20b42b54120d202ea0e9
aarvin roshin <144612896+aarvinr@users.noreply.github.com>
2024-05-06 23:21:29 +0000

    style: rename to LOLCatCompiler

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "tempLang",
+        .name = "LOLCatCompiler",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
@@ -20,7 +20,7 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the compiler");
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
@@ -28,7 +28,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    
+
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
