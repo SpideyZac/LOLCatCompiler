6f25823f88c7f98b926e0b5d1687d3e85e7d5451
spideyzac <zacmlesser-7@outlook.com>
2024-05-06 17:13:16 -0700

    style: remove comments from main header

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,3 @@
-// DYNAMIC TYPE RESOURCES
-// https://ziglang.org/documentation/master/#Function-Parameter-Type-Inference
-// https://ikrima.dev/dev-notes/zig/zig-metaprogramming/
-// https://ziglang.org/documentation/master/#Generic-Data-Structures
-
 const std = @import("std");
 
 const types = @import("lib/compiler/types.zig");
